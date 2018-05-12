4e8294fa67a9e5470c2e353318e12850d6c47b76
elias jr <elias.dvlpr@gmail.com>
2018-05-12 19:26:03 -0300

    full front loja virtual

---
+++ src/app/modules/form-validator/form-validator.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { FormValidator } from 'app/modules/form-validator/form-validator.component';
+
+@NgModule({
+  imports: [],
+  declarations: [ FormValidator ],
+  exports: [ FormValidator ],
+  providers: [],
+})
+
+export class FormValidatorModule {}
